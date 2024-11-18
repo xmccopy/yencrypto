a531d39cd8d3d1035defed65370a2566c8486e68
Monte Cristo <eugene20920@gmail.com>
2024-11-19 04:33:53 +0900

    [Add] spinner

--- pages/components/ApplyForm.tsx
+++ pages/components/ApplyForm.tsx
@@ -16,13 +16,20 @@ interface TrackingTokenPriceProps {
   currentTime: string;
 }
 
+const cryptoLabel: any = {
+  BTC: "必要なビットコイン数:",
+  ETH: "必要なイーサリアム:",
+  LTC: "必要なUSDT:",
+};
+
 const CombinedForm = () => {
-  const [tokenType, setTokenType] = useState("BTC");
+  const [tokenType, setTokenType] = useState<any>("BTC");
   const [usdPrice, setPrice] = useState(1);
   const [jpyPice, setJPYPrice] = useState(0);
+  const [jpyValue, setJpyValue] = useState("");
+  const [loading, setLoading] = useState(false);
   const [calcPrice2, setCalcPrice2] = useState(0);
   const [currentTime, setCurrentTime] = useState("");
-  const [jpyValue, setJpyValue] = useState("");
 
   const updateTime = () => {
     const now = new Date();
@@ -114,14 +121,21 @@ const CombinedForm = () => {
       //   .integer(),
     }),
     onSubmit: async (values) => {
-      if (!jpyValue) {
+      if (!Number(jpyValue)) {
         toast("金額を入力してください", {
           type: "error",
           theme: "colored",
         });
         return;
       }
-
+      if (!Number(calcPrice2)) {
+        toast("まず計算してください", {
+          type: "error",
+          theme: "colored",
+        });
+        return;
+      }
+      setLoading(true);
       try {
         const response = await fetch(
           "https://6900-104-180-21-85.ngrok-free.app/send-application",
@@ -137,9 +151,13 @@ const CombinedForm = () => {
             }),
           }
         );
-        const result = await response.json();
-        console.log(result);
+        setLoading(false);
+        toast("申請フォームが正常に送信されました。!", {
+          type: "success",
+          theme: "colored",
+        });
       } catch (error) {
+        setLoading(false);
         console.error("Error:", error);
       }
       // console.log(values);
@@ -364,7 +382,7 @@ const CombinedForm = () => {
 
         <div>
           <label className="text-[12px] font-semibold text-[#212121]">
-            必要なビットコイン数:
+            {cryptoLabel[tokenType]}
           </label>
 
           <div className="border border-[#185F03] rounded-lg h-[39px] w-full bg-[#FEFEFE] px-[12px] flex items-center justify-between">
@@ -406,9 +424,28 @@ const CombinedForm = () => {
       <div className="flex justify-center">
         <button
           type="submit"
-          className="mt-3 py-2 px-4 bg-gray-700 text-white rounded-md"
+          className="mt-3 py-2 px-4 bg-gray-700 text-white rounded-md w-[160px] items-center justify-center flex"
         >
-          申し込み内容送信
+          {loading ? (
+            <svg
+              aria-hidden="true"
+              className="w-[24px] h-[24px] text-gray-200 animate-spin dark:text-gray-600 fill-white"
+              viewBox="0 0 100 101"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="currentColor"
+              />
+              <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentFill"
+              />
+            </svg>
+          ) : (
+            "申し込み内容送信"
+          )}
         </button>
       </div>
       <ToastContainer />
