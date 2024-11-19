f06e64b2e1df310d6e196334bcb3a4f2b1a22423
xcomacdev <johnsuzuki898@gmail.com>
2024-11-19 09:23:36 +0900

    fix : type error

--- pages/components/ApplyForm.tsx
+++ pages/components/ApplyForm.tsx
@@ -4,52 +4,19 @@ import { useFormik } from "formik";
 import * as Yup from "yup";
 import TextInput from "./TextInput";
 import { useEffect, useMemo, useState } from "react";
-import { NumericFormat } from "react-number-format";
-import { CopyToClipboard } from "react-copy-to-clipboard";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
 interface TrackingTokenPriceProps {
   currency: string;
   usdPrice: number;
-  jpyPrice: number;
-  currentTime: string;
-}
-
-const cryptoLabel: Record<string, string> = {
-  BTC: "必要なビットコイン数:",
-  ETH: "必要なイーサリアム:",
-  LTC: "必要なUSDT:",
-};
-
-interface FormValues {
-  firstName: string;
-  lastName: string;
-  email: string;
-  phone: string;
-  applicationType: string;
-  csid: string;
-  participantCount: number;
-  cryptoType: string;
+  jpyPice: number;
 }
 
 const CombinedForm = () => {
-  const [tokenType, setTokenType] = useState<string>("BTC");
-  const [usdPrice, setUsdPrice] = useState<number>(1);
-  const [jpyPrice, setJPYPrice] = useState<number>(0);
-  const [jpyValue, setJpyValue] = useState<string>("");
-  const [loading, setLoading] = useState<boolean>(false);
-  const [calcPrice2, setCalcPrice2] = useState<number>(0);
-  const [currentTime, setCurrentTime] = useState<string>("");
-
-  const updateTime = () => {
-    const now = new Date();
-    const hours = String(now.getHours()).padStart(2, "0");
-    const minutes = String(now.getMinutes()).padStart(2, "0");
-    const seconds = String(now.getSeconds()).padStart(2, "0");
-
-    setCurrentTime(`${hours}:${minutes}:${seconds}`);
-  };
+  const [tokenType, setTokenType] = useState("");
+  const [usdPrice, setPrice] = useState(1);
+  const [jpyPice, setJPYPrice] = useState(0);
+  const [calcPrice1, setCalcPrice1] = useState(0);
+  const [calcPrice2, setCalcPrice2] = useState(0);
 
   const currency = useMemo(() => {
     if (!tokenType || tokenType === "LTC") return "USDT";
@@ -58,32 +25,30 @@ const CombinedForm = () => {
 
   const handleGetPrice = async (token: string) => {
     if (token === "BTC" || token === "ETH") {
-      try {
-        const response = await fetch(
-          `https://api.binance.com/api/v3/ticker/price?symbol=${token}USDT`
-        );
+      const response = await fetch(
+        `https://min-api.cryptocompare.com/data/price?fsym=${
+          token || "BTC"
+        }&tsyms=USD`
+      );
 
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        const result = await response.json();
-        updateTime();
-        setUsdPrice(Number(Number(result?.price).toFixed(0)) || 0);
-      } catch (error) {
-        console.log(error);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
       }
+      const result = await response.json();
+      setPrice(Number(Number(result?.USD).toFixed(0)) || 0);
     } else {
-      setUsdPrice(1);
+      setPrice(1);
     }
   };
 
   const handleGetJPYPrice = async () => {
-    const response = await fetch(`https://api.exchangerate-api.com/v4/latest/USD`);
+    const response = await fetch(
+      `https://api.exchangerate-api.com/v4/latest/USD`
+    );
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
     const data = await response.json();
-    updateTime();
     setJPYPrice(Number(Number(data.rates.JPY).toFixed(0)));
   };
 
@@ -97,7 +62,7 @@ const CombinedForm = () => {
     return () => clearInterval(intervalId);
   }, [tokenType]);
 
-  const formik = useFormik<FormValues>({
+  const formik = useFormik({
     initialValues: {
       firstName: "",
       lastName: "",
@@ -105,56 +70,44 @@ const CombinedForm = () => {
       phone: "",
       applicationType: "",
       csid: "",
-      participantCount: 0,
-      cryptoType: "BTC",
+      participantCount: "",
+      cryptoType: "",
+      amount: "",
     },
     validationSchema: Yup.object({
       firstName: Yup.string().required("氏名は必須です"),
       lastName: Yup.string().required("姓は必須です"),
-      email: Yup.string().email("無効なメールアドレスです").required("メールアドレスは必須です"),
+      email: Yup.string()
+        .email("無効なメールアドレスです")
+        .required("メールアドレスは必須です"),
       phone: Yup.string().required("電話番号は必須です"),
       applicationType: Yup.string().required("申し込みプランは必須です"),
       csid: Yup.string().required("CSIDは必須です"),
-      participantCount: Yup.number().required("申し込み権利数は必須です").positive().integer(),
+      participantCount: Yup.number()
+        .required("申し込み権利数は必須です")
+        .positive()
+        .integer(),
       cryptoType: Yup.string().required("仮想通貨を選択してください"),
+      amount: Yup.number()
+        .required("金額を入力してください")
+        .positive()
+        .integer(),
     }),
-    onSubmit: async (values) => {
-      if (!Number(jpyValue)) {
-        toast("金額を入力してください", { type: "error", theme: "colored" });
-        return;
-      }
-      if (!Number(calcPrice2)) {
-        toast("まず計算してください", { type: "error", theme: "colored" });
-        return;
-      }
-      setLoading(true);
-      try {
-        const response = await fetch("https://6900-104-180-21-85.ngrok-free.app/send-application", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            ...values,
-            amount: jpyValue,
-            crypto_amount: calcPrice2,
-          }),
-        });
-        setLoading(false);
-        toast("申請フォームが正常に送信されました。!", { type: "success", theme: "colored" });
-      } catch (error) {
-        setLoading(false);
-        console.error("Error:", error);
-      }
+    onSubmit: (values) => {
+      // Handle form submission
+      console.log(values);
     },
   });
 
   const handleCalc = () => {
-    if (!jpyValue || !tokenType) return;
+    if (!formik.values.amount || !tokenType) return;
 
-    const usdAmount = Number(jpyValue) / jpyPrice;
+    const usdAmount = Number(formik.values.amount) / jpyPice;
     let tokenValue = usdAmount / usdPrice;
 
     if (tokenValue > 1000) tokenValue = Number(tokenValue.toFixed(0));
 
+    setCalcPrice1(Number(formik.values.amount));
     setCalcPrice2(tokenValue);
   };
 
@@ -165,7 +118,11 @@ const CombinedForm = () => {
         name="firstName"
         value={formik.values.firstName}
         onChange={formik.handleChange}
-        error={formik.touched.firstName && formik.errors.firstName ? formik.errors.firstName : undefined}
+        error={
+          formik.touched.firstName && formik.errors.firstName
+            ? formik.errors.firstName
+            : undefined
+        }
       />
 
       <TextInput
@@ -173,7 +130,11 @@ const CombinedForm = () => {
         name="lastName"
         value={formik.values.lastName}
         onChange={formik.handleChange}
-        error={formik.touched.lastName && formik.errors.lastName ? formik.errors.lastName : undefined}
+        error={
+          formik.touched.lastName && formik.errors.lastName
+            ? formik.errors.lastName
+            : undefined
+        }
       />
 
       <TextInput
@@ -181,7 +142,11 @@ const CombinedForm = () => {
         name="email"
         value={formik.values.email}
         onChange={formik.handleChange}
-        error={formik.touched.email && formik.errors.email ? formik.errors.email : undefined}
+        error={
+          formik.touched.email && formik.errors.email
+            ? formik.errors.email
+            : undefined
+        }
       />
 
       <TextInput
@@ -189,39 +154,59 @@ const CombinedForm = () => {
         name="phone"
         value={formik.values.phone}
         onChange={formik.handleChange}
-        error={formik.touched.phone && formik.errors.phone ? formik.errors.phone : undefined}
+        error={
+          formik.touched.phone && formik.errors.phone
+            ? formik.errors.phone
+            : undefined
+        }
       />
 
-      <div className="bg-[#F3F3F3] rounded-[16px] px-[32px] py-[24px]">
-        <label className="font-bold text-[14px] text-[#212121]">
+      <div className="mb-4 p-4 bg-gray-300">
+        <label className="font-bold text-[14px]">
           申し込みプラン<span className="text-red-500 text-[10px]"> ※必須</span>
         </label>
         <div className="mt-1">
-          <label className="mr-4 text-[#212121]">
-            <input type="radio" name="applicationType" value="individual" onChange={formik.handleChange} />
+          <label className="mr-4">
+            <input
+              type="radio"
+              name="applicationType"
+              value="individual"
+              onChange={formik.handleChange}
+            />
             スポーツデータイム
           </label>
-          <label className="text-[#212121]">
-            <input type="radio" name="applicationType" value="light" onChange={formik.handleChange} />
+          <label>
+            <input
+              type="radio"
+              name="applicationType"
+              value="light"
+              onChange={formik.handleChange}
+            />
             ライトプラン
           </label>
         </div>
-        <label className="text-[10px] font-light text-[#212121]">
+        <label className="text-[10px] font-light">
           ※ライトプランは、初回お申し込みのお客様のみ選択可能です。
         </label>
         {formik.touched.applicationType && formik.errors.applicationType ? (
-          <div className="text-red-500 mt-1">{formik.errors.applicationType}</div>
+          <div className="text-red-500 mt-1">
+            {formik.errors.applicationType}
+          </div>
         ) : null}
       </div>
 
       <TextInput
         label="申し込み権利数"
         name="participantCount"
-        type="number"
-        value={formik.values.participantCount as number}
+        value={formik.values.participantCount as unknown as string}
         onChange={formik.handleChange}
         required
-        error={formik.touched.participantCount && formik.errors.participantCount ? formik.errors.participantCount : undefined}
+        need
+        error={
+          formik.touched.participantCount && formik.errors.participantCount
+            ? formik.errors.participantCount
+            : undefined
+        }
       />
 
       <TextInput
@@ -229,88 +214,163 @@ const CombinedForm = () => {
         name="csid"
         value={formik.values.csid}
         onChange={formik.handleChange}
-        error={formik.touched.csid && formik.errors.csid ? formik.errors.csid : undefined}
+        error={
+          formik.touched.csid && formik.errors.csid
+            ? formik.errors.csid
+            : undefined
+        }
       />
 
       <hr />
 
-      <h2 className="mt-6 text-[12px] font-light">ご入金時にご利用いただく仮想通貨をご選択ください。</h2>
-      <div className="bg-[#F3F3F3] rounded-[16px] px-[32px] py-[24px]">
-        <div className="flex flex-wrap items-center gap-2">
-          <button
-            type="button"
-            className={`py-2 px-4 rounded-md ${
-              tokenType === "BTC" ? "bg-[#006EFF] text-white" : "bg-[#E1EFFF]"
+      <h2 className="mt-6 text-[12px] font-light">
+        ご入金時にご利用いただく仮想通貨をご選択ください。
+      </h2>
+      <div className="mb-4 p-4 bg-gray-300">
+        <label htmlFor="cryptoType" className="block text-[14px] font-semibold">
+          仮想通貨
+        </label>
+        <select
+          id="cryptoType"
+          name="cryptoType"
+          value={formik.values.cryptoType}
+          onChange={(e) => {
+            setCalcPrice1(0);
+            setTokenType(e.target.value);
+            formik.handleChange(e);
+          }}
+          className={`mt-2 block w-full p-2 border rounded-md text-[14px] ${
+            formik.touched.cryptoType && formik.errors.cryptoType
+              ? "border-red-500"
+              : "border-gray-300"
+          }`}
+        >
+          <option value="" className="text-[14px]">
+            選択してください
+          </option>
+          <option value="BTC" className="text-[14px]">
+            ビットコイン (BTC)
+          </option>
+          <option value="ETH" className="text-[14px]">
+            イーサリアム (ETH)
+          </option>
+          <option value="LTC" className="text-[14px]">
+            テザー (USDT)
+          </option>
+        </select>
+        {formik.touched.cryptoType && formik.errors.cryptoType ? (
+          <div className="text-red-500 mt-1">{formik.errors.cryptoType}</div>
+        ) : null}
+      </div>
+      <div>
+        <label className="text-[12px] font-semibold">
+          仮想通貨入金額は、お客様が申し込み手続きをされた時点
+          の市場レートで計算されます。
+        </label>
+      </div>
+      <div className="mb-4 p-4 bg-gray-300">
+        <label className="text-[12px] font-semibold">仮想通貨数量を計算</label>
+        <label className="mt-3 block text-[12px] font-semibold">
+          日本円入金額を入力
+        </label>
+        <div className="flex flex-row gap-1 items-end">
+          <input
+            id="amount"
+            name="amount"
+            type="number"
+            value={formik.values.amount}
+            onChange={formik.handleChange}
+            placeholder="日本円金額を入力"
+            className={`mt-2 block w-full p-2 border rounded-md text-[14px] ${
+              formik.touched.amount && formik.errors.amount
+                ? "border-red-500"
+                : "border-gray-300"
             }`}
-            onClick={() => setTokenType("BTC")}
-          >
-            BTC
-          </button>
-          <button
-            type="button"
-            className={`py-2 px-4 rounded-md ${
-              tokenType === "ETH" ? "bg-[#006EFF] text-white" : "bg-[#E1EFFF]"
-            }`}
-            onClick={() => setTokenType("ETH")}
-          >
-            ETH
-          </button>
-          <button
-            type="button"
-            className={`py-2 px-4 rounded-md ${
-              tokenType === "LTC" ? "bg-[#006EFF] text-white" : "bg-[#E1EFFF]"
-            }`}
-            onClick={() => setTokenType("LTC")}
-          >
-            LTC
-          </button>
-        </div>
-        <div className="mt-2">
-          <label className="font-bold text-[14px] text-[#212121]">{cryptoLabel[tokenType]}</label>
-          <NumericFormat
-            className="input-style"
-            value={jpyValue}
-            onValueChange={(values) => setJpyValue(values.value)}
-            thousandSeparator
-            prefix="¥"
-            decimalScale={0}
           />
+          <span>円</span>
         </div>
-        <div className="mt-4 flex gap-4">
+        {formik.touched.amount && formik.errors.amount ? (
+          <div className="text-red-500 mt-1">{formik.errors.amount}</div>
+        ) : null}
+
+        <div className="flex items-center gap-2 mt-4 ">
           <button
             type="button"
+            className="py-1 px-4 bg-gradient-to-r from-[#c27070] to-[#0dad0d] p-10 text-white rounded-full text-[12px]"
             onClick={handleCalc}
-            className="py-2 px-4 bg-[#006EFF] text-white rounded-md"
           >
             計算する
           </button>
-          <div className="mt-2">
-            <p className="font-bold">必要な{tokenType}:</p>
-            <p className="font-semibold">{calcPrice2}</p>
-          </div>
+          {calcPrice1 ? (
+            <p className="text-[12px] font-light">
+              {calcPrice1} 円 = {calcPrice2} {currency}
+            </p>
+          ) : null}
         </div>
+
+        <label className="mt-3 block text-[10px] font-light">
+          ※日本円入金額を入力し、計算するボタンを押してください。
+        </label>
+
+        <label className="mt-3 block text-[12px] font-light">
+          仮想通貨入金額は、お客様が申し込み手続きされた時点の市場
+          レートで計算されます。
+        </label>
       </div>
 
-      <div className="flex justify-end space-x-4">
-        <button
-          type="button"
-          className="bg-[#F1F1F1] py-2 px-6 rounded-[20px] text-[12px]"
-          onClick={() => formik.resetForm()}
-        >
-          リセット
-        </button>
+      <TrackingTokenPrice
+        currency={currency}
+        usdPrice={usdPrice}
+        jpyPice={jpyPice}
+      />
+
+      <div className="flex justify-center">
         <button
           type="submit"
-          className="bg-[#006EFF] py-2 px-6 rounded-[20px] text-[12px] text-white"
-          disabled={loading}
+          className="mt-3 py-2 px-4 bg-gray-700 text-white rounded-md"
         >
-          送信
+          申し込み内容送信
         </button>
       </div>
-
-      <ToastContainer position="bottom-left" autoClose={3000} />
     </form>
   );
+};
+
+const TrackingTokenPrice = ({ currency, usdPrice, jpyPice }: TrackingTokenPriceProps) => {
+  const calcJPYPrice = useMemo(() => usdPrice * jpyPice, [usdPrice, jpyPice]);
+
+  return (
+    <div className="mt-4 text-[12px] font-light">
+      <p>
+        1 {currency} = ¥{calcJPYPrice}
+      </p>
+      <CountdownTimer />
+    </div>
+  );
+};
+
+const CountdownTimer = () => {
+  const [currentTime, setCurrentTime] = useState("");
+
+  useEffect(() => {
+    const updateTime = () => {
+      const now = new Date();
+      const hours = String(now.getHours()).padStart(2, "0");
+      const minutes = String(now.getMinutes()).padStart(2, "0");
+      const seconds = String(now.getSeconds()).padStart(2, "0");
+
+      // Format the time as HH:MM:SS
+      setCurrentTime(`${hours}:${minutes}:${seconds}`);
+    };
+
+    // Update time immediately and then every second
+    updateTime();
+    const intervalId = setInterval(updateTime, 1000);
+
+    return () => clearInterval(intervalId); // Cleanup on unmount
+  }, []);
+
+  return <p>【レート取得時刻】{currentTime}</p>;
 };
 
 export default CombinedForm;
