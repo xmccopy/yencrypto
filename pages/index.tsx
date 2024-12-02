739303b41e483118f968ce8ac36cba8c1cb1a28d
Monte Cristo <111125255+monte1s@users.noreply.github.com>
2024-12-02 10:49:49 +0900

    [Update] link

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,23 +17,13 @@ export default function Home() {
             下記のフォームをご入力ください。ご入金時のBTC数量は、以下に表示される数量となります
           </p>
           <ApplyForm />
-          <a href="#" className="mt-14 block underline">
-            こちらも併せてご確認ください
-          </a>
-          <div className="flex flex-row gap-1 items-center">
+          <div className="flex flex-row gap-1 items-center mt-14">
             <p className="text-[12px] font-light">
               <a
-                href="https://bitflyer.com/en-jp/s/how-to-start"
+                href="https://masterfund-mgmt.com/doc/accountguide.pdf"
                 target="_blank"
               >
-                bitFlyer口座開設方法
-              </a>
-              ・
-              <a
-                href="https://trustwallet.com/blog/the-official-beginners-guide-to-trust-wallet"
-                target="_blank"
-              >
-                Trust Walletアカウント設定方法
+                bitFlyer口座開設方法 ・ Trust Walletアカウント設定方法
               </a>
             </p>
             <a href="">
@@ -43,7 +33,7 @@ export default function Home() {
           <div className="flex flex-row gap-1 items-center">
             <p className="text-[12px] font-light">
               <a
-                href="https://www.japan-guide.com/e/e2226.html"
+                href="https://masterfund-mgmt.com/doc/transactionguide.pdf"
                 target="_blank"
               >
                 日本在住者向け・入出金ガイド
